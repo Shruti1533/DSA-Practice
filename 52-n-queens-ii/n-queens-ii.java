fc2c87d67cb0fce6a1d64e2fa90e7db18dae6c31
Shruti1533 <148115188+Shruti1533@users.noreply.github.com>
2024-05-25 17:08:34 +0530

    Time: 2 ms (54.28%) | Memory: 40 MB (78.04%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.java
@@ -0,0 +1,53 @@
+class Solution {
+    static boolean b[][];
+    public int totalNQueens(int n) {
+        b=new boolean [n][n];
+        return helper(0,n);
+    }
+
+    public static int helper(int row,int n){
+
+        //Base case
+        if(row==n){
+            return 1;
+        }
+
+        int count=0;
+
+        for(int col=0;col<n;col++){
+            if(isSafe(row,col)){
+                b[row][col]=true;//Queen placed
+                //Move to necxt zero and col=0
+                count+=helper(row+1,n);
+
+                b[row][col]=false;//Undo operation
+            }
+        }
+        return count;
+    }
+
+    public static boolean isSafe(int row, int col){
+            //Above row
+            for(int i=row;i>=0;i--){
+                if(b[i][col]==true){
+                    return false;
+                }
+            }
+        
+        //Left diagonal
+         for(int i=row,j=col;i>=0 && j>=0;i--, j--){
+                if(b[i][j]==true){
+                    return false;
+                }
+            }
+        //Right diagonal
+        for(int i=row,j=col;i>=0 && j<b[0].length;i--, j++){
+                if(b[i][j]==true){
+                    return false;
+                }
+            }
+        
+        return true;
+    }
+}
+
