ce6e86dc6a79416b41d67e55f9bcc14ba01a3eb9
Shruti1533 <148115188+Shruti1533@users.noreply.github.com>
2024-07-06 07:05:00 +0530

    Time: 7 ms (42.56%) | Memory: 45.3 MB (26.49%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack <Integer> stack=new Stack<>();
+        boolean same = false;
+        for(int i=0;i<asteroids.length;i++){
+            same=false;
+
+            //Checking collisiom
+            while(!stack.isEmpty() && asteroids[i]<0 && stack.peek()>0){
+                
+                //if coming asteroid is greater than previous
+                if(Math.abs(asteroids[i])>Math.abs(stack.peek())){
+                    stack.pop();
+                    continue;
+                }
+                
+                //is same size
+                else if(Math.abs(asteroids[i])==stack.peek()){
+                    stack.pop();
+                }
+
+                //if in same direction
+                same=true;
+                break;
+            }   
+
+            if(!same){
+            stack.push(asteroids[i]);
+            }
+        }
+
+        int [] result=new int[stack.size()];
+        for(int i=stack.size()-1;i>=0;i--){
+            result[i]=stack.pop();
+        }
+
+        return result;
+    }
+}
